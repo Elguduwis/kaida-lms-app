48f8a09b682c17776386b0e4f7cfba922e4a4612
Osama Elguduwis <44493627+Elguduwis@users.noreply.github.com>
2026-03-23 00:56:18 +0100

    Delete kaida_app directory

--- kaida_app/lib/config/api_config.dart
+++
@@ -1,7 +0,0 @@
-class ApiConfig {
-  static const String baseUrl = 'https://academy.kainuwa.africa/api/mobile';
-  
-  static const String login = '$baseUrl/login.php';
-  static const String register = '$baseUrl/register.php';
-  static const String courses = '$baseUrl/courses.php';
-}
